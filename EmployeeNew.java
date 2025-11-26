00ad9fa5372b85f3146862205295f7996ab9ebcc
Jaimere10 <sonicnshadow@outlook.com>
2025-11-26 11:05:24 -0500

    Add files via upload

---
+++ EmployeeNew.java
@@ -0,0 +1,75 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeNew {
+    private int empid;
+    private String name;
+    private String SSN;
+    private double salary;
+    private String jobTitle;
+    private String division;
+    private String email;
+
+
+    public EmployeeNew(int empid, String name, String SSN, double salary, String jobTitle, String division, String email){
+        this.empid = empid;
+        this.name = name;
+        this.SSN = SSN;
+        this.salary = salary;
+        this.jobTitle = jobTitle;
+        this.division = division;
+        this.payHistory = payHistory;
+    }
+
+    public int getEmpid() {
+        return empid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSSN() {
+        return SSN;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public String getDivision() {
+        return division;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSSN(String SSN) {
+        this.SSN = SSN;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public void setDivision(String division) {
+        this.division = division;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
