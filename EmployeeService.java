e4f8e393153f6190215333ae08df326fcd3c373b
Jaimere10 <sonicnshadow@outlook.com>
2025-11-29 18:58:01 -0500

    Update EmployeeService.java

--- EmployeeService.java
+++ EmployeeService.java
@@ -1,9 +1,41 @@
-public class EmployeeService 
-{
-    EmployeeRepository repository;
+import java.util.ArrayList;
+import java.util.List;
 
-    void updateSalary(double percentage, double minSalary, double maxSalary)
-    {
+public class EmployeeService {
+    protected IEmployeeRepository employeeRepository;
 
+    public EmployeeService(IEmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.getAllEmployees();
+    }
+
+    public Employee searchEmployee(String criteria) {
+        return employeeRepository.searchEmployee(criteria);
+    }
+
+    public void updateEmployee(Employee employee) {
+        employeeRepository.updateEmployee(employee);
+    }
+
+    public void addEmployee(Employee employee) {
+        employeeRepository.addEmployee(employee);
+    }
+
+    public void deleteEmployee(int id) {
+        employeeRepository.deleteEmployee(id);
+    }
+
+    public void updateSalary(double percentage, double minSalary, double maxSalary) {
+        List<Employee> employees = employeeRepository.getAllEmployees();
+        for (Employee emp : employees) {
+            if (emp.getSalary() >= minSalary && emp.getSalary() <= maxSalary) {
+                double newSalary = emp.getSalary() + (emp.getSalary() * percentage / 100);
+                emp.setSalary(newSalary);
+                employeeRepository.updateEmployee(emp);
+            }
+        }
     }
 }
