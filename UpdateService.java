c13d9419e8e86195df462da3e26cc06b18d01b39
Jaimere10 <sonicnshadow@outlook.com>
2025-11-29 19:02:37 -0500

    Update UpdateService.java

--- UpdateService.java
+++ UpdateService.java
@@ -1,9 +1,26 @@
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
-public class UpdateService extends EmployeeService
-{
-    void updateEmployeeData(int empID, Map<String, Object> newData)
-    {
-
+public class UpdateService extends EmployeeService {
+    public UpdateService(IEmployeeRepository employeeRepository) {
+        super(employeeRepository);
+    }
+    public void updateEmployeeData(int empid, String newName, String newJobTitle, String newDivision) {
+        Employee employee = employeeRepository.searchEmployee(String.valueOf(empid));
+        if (employee != null) {
+            if (newName != null && !newName.isEmpty()) {
+                employee.setName(newName);
+            }
+            if (newJobTitle != null && !newJobTitle.isEmpty()) {
+                employee.setJobTitle(newJobTitle);
+            }
+            if (newDivision != null && !newDivision.isEmpty()) {
+                employee.setDivision(newDivision);
+            }
+            employeeRepository.updateEmployee(employee);
+            System.out.println("Updated Employee ID: " + empid);
+        } else {
+            System.out.println("Employee with ID " + empid + " not found.");
+        }
     }
 }
